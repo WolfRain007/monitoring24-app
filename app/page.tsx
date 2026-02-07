c54ccf69525aac1d69a091a1fd0138b6b4f7f262
WolfRain007 <101939907+WolfRain007@users.noreply.github.com>
2026-02-07 13:55:12 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
+import { EnvWarning } from "@/components/env-warning";
 import { AuthButton } from "@/components/auth-button";
 import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
