9e8a9d9304c8fb371f0e81af078d184eaea7f28b
WolfRain007 <101939907+WolfRain007@users.noreply.github.com>
2026-03-26 22:02:12 +0300

    Update proxy.tsв

--- proxy.ts
+++ proxy.ts
@@ -1,20 +1,10 @@
 import { updateSession } from "@/lib/supabase/proxy";
-import { NextResponse, type NextRequest } from "next/server";
+import { type NextRequest } from "next/server";
 
 export async function proxy(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  // ЖЁСТКО пропускаем все API-роуты
-  if (pathname.startsWith("/api/")) {
-    return NextResponse.next();
-  }
-
   return await updateSession(request);
 }
 
 export const config = {
-  matcher: [
-    // матчим всё, но API пропускаем внутри proxy()
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
+  matcher: ["/protected/:path*"],
 };
